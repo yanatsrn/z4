2d2dce3b26331402f13f3af4e0f6a43220c55c47
yanatsrn <54328632+yanatsrn@users.noreply.github.com>
2019-08-26 00:17:04 +0300

    степень

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int n = 3;
+        System.out.println("Полученное выражение равно = " + (a*a*a));
+        //  а как не вручную это сделать
+
+
+    }
+}
